095766db2aeeacc85c0d633651db84f24bdd2a5b
Ashlynn Wood <120209167+ashlynnwood@users.noreply.github.com>
2023-01-05 22:32:55 -0600

    Added questions and answers content

--- script.js
+++ script.js
@@ -3,22 +3,23 @@
 var startQuiz = document.querySelector("#start-btn");
 startQuiz.addEventListener("click", function() {
   // When click button, empty quiz challenge div
-  document.getElementById("quiz-card").innerHTML = "";
+  document.getElementById("qDiv").innerHTML = "";
   }
 );
 
 // when click start button: timer starts
 var timeEl = document.querySelector(".time");
 var timerEl = document.getElementById("#timer");
-var countDown = 5;
+var countDown = 76;
 
 function setTime() {
   var timerCount = setInterval(function() {
     countDown--;
-    timeEl.textContent = timerEl + countDown;
+    timeEl.textContent = "Time: " + countDown;
 
     if(countDown === 0) {
       clearInterval(timerCount);
+      timeEl.textContent = "Time's up!";
     }
   }, 1000);
 }
@@ -30,17 +31,21 @@ startQuiz.addEventListener("click", function() {
 
 // set inner html of div to string so it will go away on click start
 // innerhtml or text content to empty string often
-
-// then use another function to show osmething on the page (first question)
+// then use another function to show something on the page (first question)
 
 // start running several functions
 // 1. timer
-// 2. updating the oage (next question)
+// 2. updating the page (next question)
 // 3. getting answer (when click one, run another function)
 // separation of concerns- break down
 // 3. click answer- get value off button -> data attributes
 
 // Clear highscores- empties the div
+var clearScores = document.getElementById("#clear");
+clearScores.addEventListener("click", function () {
+  document.getElementById("#highscores").innerHTML = "";
+}
+);
 
 // clicking a button
 // starting a timer
@@ -55,14 +60,35 @@ startQuiz.addEventListener("click", function() {
 // 3. correct answer
 
 var QIndex = 0;
+var userScore = 0;
 
 var questions = [
   {
-    q: "Question?",
-    answers: ["one", "two", "three", "four"],
-    a: "one"
-  }
-]
+    q: "The concat method combines which items?",
+    answers: ["Arrays", "Functions", "Booleans", "Numbers"],
+    a: "Arrays"
+  },
+  {
+    q: "What does the strict equality operator compare?",
+    answers: ["Type", "Value", "Both type and value", "None of the above"],
+    a: "Both type and value"
+  },
+  {
+    q: "What method should you use during development to make sure you are targeting what you want?",
+    answers: ["function()", "console.log()", "debug()", "array.length"],
+    a: "console.log()"
+  },
+  {
+    q: "What is a for loop used for?",
+    answers: ["Repeat functions", "Loop methods", "Log to the console", "Iterate over arrays"],
+    a: "Iterate over arrays"
+  },
+  {
+    q: "How do you create a function in JavaScript?",
+    answers: ["function functionName()", "function: functionName", "function = functionName", "none of the above"],
+    a: "function functionName()"
+  },
+];
 
 // Loop over these questions- put question in question div
 // Loop over answers
@@ -72,6 +98,7 @@ start = (i) => {
   // pass in question index 
   qDiv.textContent = questions[i].a
   // loop over the questions[i].answers
+  for (var i = 0; i < questions.length; i++)
   // Append these to the answers div
   // aDiv.textContent = 
   // loop over answers and append --NOT A FOR LOOP
